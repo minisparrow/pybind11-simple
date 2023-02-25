b033db98b0c60d95611ec2b1a44c42a2e2478ad7
minisparrow <melchior_faith@163.com>
2023-02-25 10:37:29 +0800

    first commit

---
+++ robot.cpp
@@ -0,0 +1,24 @@
+#include <pybind11/pybind11.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Root{
+public:
+    Robot(const std::string &name) : name(name) { }
+
+    void do_task(const std::string &task) {
+        std::cout << name << "is doing " << task << std::endl;
+    }
+
+    std::string name;
+}
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(robot, m) {
+    py::class_<Robot>(m, "Robot") 
+        .def(py::init<const std:string &>())
+        .def("do_task", &Robot::do_task)
+        .def_readwrite("name", &Robot::name);
+}
